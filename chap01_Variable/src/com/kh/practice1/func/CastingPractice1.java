d7d28f862c0279c5c44b8ae2d3ac3e05d98a4fcc
rjswh0503 <rjswh0503@yahoo.com>
2023-08-31 09:05:08 +0900

    scanner casting pratice

--- chap01_Variable/src/com/kh/practice1/func/CastingPractice1.java
+++ chap01_Variable/src/com/kh/practice1/func/CastingPractice1.java
@@ -5,13 +5,13 @@ import java.util.Scanner;
 public class CastingPractice1 {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
-		System.out.print("문자 : ");
+		System.out.print(" 문자 : ");
 		char ch = sc.nextLine().charAt(0);
 		int num = (int)ch;
 	
 		System.out.println("A unicode : " + num);
 		
-		System.out.print("문자 : ");
+		System.out.print(" 문자 : ");
 		char ch1 = sc.nextLine().charAt(0);
 		int num1 = (int)ch1;
 		
