ed813146e65b929e902b0c6b73a0c978e7717c13
rjswh0503 <rjswh0503@yahoo.com>
2023-09-08 08:59:52 +0900

    Practice

---
+++ Practice/src/com/kh/practice/dimension/DimensionPractice.java
@@ -0,0 +1,18 @@
+package com.kh.practice.dimension;
+
+public class DimensionPractice {
+	public static void main(String[] args) {
+		String[][] arr = new String[3][3];
+		for(int i = 0; i < arr.length;i++) {
+			for(int j = 0; j <arr.length;j++) {
+				System.out.print("(" + i +"," + j +")");
+			}
+			System.out.println();
+			
+			
+			
+			
+		};
+	}
+
+}
