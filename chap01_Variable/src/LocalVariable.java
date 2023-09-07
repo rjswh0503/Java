4f8a339b6c24632c26ff7c44a273052367b1a6a2
rjswh0503 <rjswh0503@yahoo.com>
2023-09-07 17:30:34 +0900

    Variable

---
+++ chap01_Variable/src/LocalVariable.java
@@ -0,0 +1,18 @@
+
+//					지역 변수 = lv로 단축해서 적음
+
+
+
+public class LocalVariable {
+
+	public static void main(String[] args) {
+		int num = 10; // 정수형 지역변수
+		String name ="kh"; //문자열 지역변수
+		System.out.print(" num : "+num + " name :  "+name);
+		
+		//다른 클래스나 파일에서는 상속을 받지 않으면 변수에 접근할 수 없다.
+
+
+	}
+
+}
---
+++ chap01_Variable/src/ReferenceVariable.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class ReferenceVariable {
+	
+	public static void main(String[] args) {
+		int num = 10;// 정수형 지역변수
+		int b = num; // 변수 값을 대입한다. b는 초기화 
+		
+/*****************************************************/
+		
+		
+		//객체(=인스턴스)
+		Scanner sc; // 객체 (= 인스턴스) 변수   참조변수
+		String s;
+		
+		final int c; // 상수
+		
+		
+		
+		
+		
+	}
+
+}
