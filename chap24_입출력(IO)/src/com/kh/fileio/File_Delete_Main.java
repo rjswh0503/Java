a5042efc723602e3c8551c622d5fb0cae99688fb
rjswh0503 <rjswh0503@yahoo.com>
2023-09-14 17:30:10 +0900

    입출력(I/O)

--- chap24_입출력(IO)/src/com/kh/fileio/File_Delete_Main.java
+++ chap24_입출력(IO)/src/com/kh/fileio/File_Delete_Main.java
@@ -5,16 +5,16 @@ import java.io.File;
 public class File_Delete_Main {
 
 	public static void main(String[] args) {
-		String mainPath = "C:\\Users\\user1\\Desktop\\img2";
-		String originFile = mainPath+"Realdd.jpg";
+		String mainPath = "C:\\Users\\user1\\Desktop\\";
+		String originFile = mainPath+"asdasdasd.txt";
 		
 		
 		
 		
 		//파일 객체 생성
-		File fileDelete = new File(originFile);
+		File fileToDelete = new File(originFile);
 		
-		boolean deleted = fileDelete.delete();
+		boolean deleted = fileToDelete.delete();
 		if(deleted) {
 			System.out.println("파일이 성공적으로 삭제되었습니다.");
 		}else {
