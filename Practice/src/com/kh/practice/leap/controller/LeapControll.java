e034a1a9e9940e7e988774e9e3f413a96b5e7b28
rjswh0503 <rjswh0503@yahoo.com>
2023-09-15 17:41:54 +0900

    Practice

---
+++ Practice/src/com/kh/practice/leap/controller/LeapControll.java
@@ -0,0 +1,10 @@
+package com.kh.practice.leap.controller;
+
+import java.util.Calendar;
+
+public class LeapControll {
+	Calendar cal = Calendar.getInstance();
+	int year = cal.get(cal.YEAR);
+	
+
+}
--- Practice/src/dd.java
+++ Practice/src/dd.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 
 public class dd {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	 public static void main(String[] args) {
+	        Scanner sc = new Scanner(System.in);
+	        System.out.print("입력 :");
+	        int num = sc.nextInt();
+	        System.out.print("입력 : ");
+	        int num1 = sc.nextInt();
+	        
+	        
+	        System.out.println("a" + "="+num + "," +  "b"+"="+ num1);
+	    
+	    
 	}
+	       
 
 }
