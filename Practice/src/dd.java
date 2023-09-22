281d30edcd4546301075c9da153b15cd888e2975
rjswh0503 <rjswh0503@yahoo.com>
2023-09-22 17:32:12 +0900

    Practice

--- Practice/src/dd.java
+++ Practice/src/dd.java
@@ -1,23 +1,13 @@
 import java.util.Scanner;
 
 public class dd {
-	public int sum(int a, int b) {
-		return a+b;
-		
-	}
 	public static void main(String[] args) {
-		int a = 3;
-		int b = 4;
 		
-		dd mytest = new dd();
-		int c = mytest.sum(a, b);
-		System.out.println(c);
+		
+		
+		
+	
+		
 		
 	}
-
-
-	
-	
-	
 }
-
